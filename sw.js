cd1ec41b2d99613873812eeb29879f8af8957d82
abdoulkdoidoi-byte <abdoulk.doidoi@gmail.com>
2026-02-17 06:47:26 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,10 @@
-const CACHE_NAME = 'abdoulk-world-v1';
-const ASSETS = [
-  './', './index.html', './manifest.json', './icon.png', 
-  './hymne-burkina.mp3', './cours-premium.pdf', './film-intro.mp4'
-];
+const CACHE_NAME = 'ab-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
-
