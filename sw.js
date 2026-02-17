444a9c1e2938f595981132220875ccef314d9b39
abdoulkdoidoi-byte <abdoulk.doidoi@gmail.com>
2026-02-17 14:44:37 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,4 @@
-const CACHE_NAME = 'ab-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+const CACHE = 'ab-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './siapi.ovpn'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
